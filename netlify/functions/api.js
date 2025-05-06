af34b111c98164293c9d26f7d800fd6d7b074cda
Ramesh Kumar <dholiwalramesh@gmail.com>
2025-05-06 23:19:36 +0530

    add data.json path

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const path = require('path');
 const fs = require('fs');
 const cors = require('cors');
 
@@ -11,7 +12,7 @@ const PORT = 5000;
 app.use(cors({ origin: ['https://expensemydiary.netlify.app', 'http://localhost:5173'] })); // Allow CORS for the specified origin
 app.use(express.json());
 
-const DATA_PATH = './data.json';
+const DATA_PATH = path.join(__dirname, 'data.json');
 
 // GET: Read from file
 router.get('/expenses', (req, res) => {
